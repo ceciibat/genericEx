9090615215aed14fe4d8e7fcaf84e1241b27e1c2
Ana Cecília <anaceciliabatalha@gmail.com>
2023-08-08 15:06:43 -0400

    Generics + Restrições de generics

---
+++ Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Fix.Entities
+{
+    internal class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Price.ToString("f2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object? obj)  // implementação da Interface IComparable
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentException("Comparing error: argument is not a Product!");
+            }
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+
+        }
+    }
+}
---
+++ Services/CalculationService.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Fix.Services
+{
+    internal class CalculationService
+    {
+        public T Max<T>(List<T> list) where T : IComparable         // a lista vai ter que ser de um tipo T qualquer desde que esse tipo seja uma implementação da itfc IComparable 
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("The list can not be empty");
+            }
+
+            T max = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].CompareTo(max) > 0)             // comparando de uma forma genérica, utilizando a interface IComparable | > 0 = Console.WriteLine("maria".CompareTo("alex")); que tem como resultado: 1
+                {
+                    max = list[i];
+                }
+            }
+            return max;
+        }
+    }
+}
